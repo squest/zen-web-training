3f62bb576180aa82408c40df97c2851121a0a44b
squest <sabdaps@gmail.com>
2015-08-28 02:07:07 +0700

    move to core

--- tutorial/alfa/src/alfa/core.clj
+++ tutorial/alfa/src/alfa/core.clj
@@ -4,10 +4,20 @@
     [alfa.stat.standard :as stat]
     [clojure.string :as cs]
     [clojure.set :as cset]
-    [clojure.edn :as edn]))
+    [clojure.edn :as edn]
+    [couchbase-clj.client :as cb]))
+
+(defonce cbdb (cb/create-client
+               {:bucket "znetroyalty"
+                :uris   ["http://127.0.0.1:8091/pools"]}))
 
 ;; clojure.core -> standard library
-;;
+
+(defn cbquery
+  [cdb design view]
+  (->> {:include-docs true}
+       (cb/query cdb design view)
+       (mapv cb/view-doc-json)))
 
 (defn open-edn
   [fname]
