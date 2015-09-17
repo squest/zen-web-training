b066948ec0adeadab9f917cfbfcba2b8456b3cd6
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-09-17 19:01:59 +0700

    awol is coming again

---
+++ awol/website1/src/website1/core.clj
@@ -0,0 +1,29 @@
+(ns website1.core
+  (:require
+    [org.httpkit.server :as http]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [noir.cookies :as cookies]))
+
+(defonce server (atom nil))
+
+(defn start
+  ([] (start 3000))
+  ([port]
+   (->> (-> (all-routes)
+            (cookies/wrap-noir-cookies*)
+            (wrap-defaults (update-in site-defaults
+                                      [:security :anti-forgery]
+                                      #(not %)))
+            (http/run-server {:port port}))
+        (reset! server))))
+
+(defn stop
+  []
+  (@server)
+  (reset! server nil))
+
+(defn reset
+  []
+  (stop)
+  (Thread/sleep 200)
+  (start))
---
+++ awol/website1/src/website1/routes.clj
@@ -0,0 +1,4 @@
+(ns website1.routes
+  (:require
+    [compojure.core :refer [routes GET POST context]]
+    [compojure.route :refer [not-found resources]]))
