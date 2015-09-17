bf5c03ab4197684de26e5a8564491c08d4c93f1f
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-09-17 20:49:00 +0700

    sync

--- awol/website1/src/website1/routes.clj
+++ awol/website1/src/website1/routes.clj
@@ -5,8 +5,10 @@
     [website1.mock :as mock]))
 
 (defn all-routes
-  []
-  (routes
-    (GET "/" req (mock/home))
-    (resources "public/")
-    (not-found "Page not found.")))
+  ([]
+   (routes
+     (GET "/" req (mock/home))
+     (resources "public/")
+     (not-found "Page not found."))))
+
+
