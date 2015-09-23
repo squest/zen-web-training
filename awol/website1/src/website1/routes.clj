12452b455b89b4409e725b13f2c5757435352a53
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-09-23 21:58:59 +0700

    sync

--- awol/website1/src/website1/routes.clj
+++ awol/website1/src/website1/routes.clj
@@ -2,12 +2,20 @@
   (:require
     [compojure.core :refer [routes GET POST context]]
     [compojure.route :refer [not-found resources]]
-    [website1.mock :as mock]))
+    [website1.mock :as mock]
+    [website1.articles :as articles]))
 
 (defn all-routes
   ([]
    (routes
      (GET "/" req (mock/home))
+     (GET "/home" req (mock/home))
+     (GET "/explanation" req (mock/explanation))
+
+     (GET "/hello/:id" [id] (str "HellooUoo " id))
+
+     (GET "/:id" [id] (articles/open id))
+
      (resources "public/")
      (not-found "Page not found."))))
 
