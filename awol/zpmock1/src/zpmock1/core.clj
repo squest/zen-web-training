a9b3ff64aa2cdbddf73eb706f08482d72b69bb9c
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-24 13:55:59 +0700

    fixing indentations

--- awol/zpmock1/src/zpmock1/core.clj
+++ awol/zpmock1/src/zpmock1/core.clj
@@ -1,31 +1,30 @@
 (ns zpmock1.core
-    (:require
-      [org.httpkit.server :as http]
-      [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-      [noir.cookies :as cookies]
-      [zpmock1.routes :refer [all-routes]]
-      ))
+  (:require
+    [org.httpkit.server :as http]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [noir.cookies :as cookies]
+    [zpmock1.routes :refer [all-routes]]))
 
 (defonce server (atom nil))
 
 (defn start
-      ([] (start 3000))
-      ([port]
-        (->> (-> (all-routes)
-                 (cookies/wrap-noir-cookies*)
-                 (wrap-defaults (update-in site-defaults
-                                           [:security :anti-forgery]
-                                           #(not %)))
-                 (http/run-server {:port port}))
-             (reset! server))))
+  ([] (start 3000))
+  ([port]
+   (->> (-> (all-routes)
+            (cookies/wrap-noir-cookies*)
+            (wrap-defaults (update-in site-defaults
+                                      [:security :anti-forgery]
+                                      #(not %)))
+            (http/run-server {:port port}))
+        (reset! server))))
 
 (defn stop
-      []
-      (@server)
-      (reset! server nil))
+  []
+  (@server)
+  (reset! server nil))
 
 (defn reset
-      []
-      (stop)
-      (Thread/sleep 200)
-      (start))
+  []
+  (stop)
+  (Thread/sleep 200)
+  (start))
